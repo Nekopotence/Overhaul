ec4a7648b6690e6a6aaaec8ce6110a4f1e7fcb4c
Jacob Bucher <necropotenceima@gmail.com>
2021-03-23 23:47:07 -0500

    Croco Changes

--- Overhaul/Class1.cs
+++ Overhaul/Class1.cs
@@ -1,8 +1,17 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
 using EntityStates.Croco;
+using EntityStates.Huntress.HuntressWeapon;
+using On.EntityStates.Huntress.HuntressWeapon;
+using RoR2.Projectile;
 using RoR2;
-using EntityStates;
-using RoR2.Skills;
+using On.RoR2;
+using R2API;
+using R2API.Utils;
+using UnityEngine;
+using UnityEngine.Networking;
+
 
 
 namespace Overhaul
@@ -10,19 +19,42 @@ namespace Overhaul
     [BepInDependency("com.bepis.r2api")]
     //Change these
     [BepInPlugin("com.Necropotence.Overhaul", "Revamped Items, Skills and Stats", "1.0.0")]
+    [R2APISubmoduleDependency(new string[]
+    {
+        "LanguageAPI"
+    })]
     public class Overhaul : BaseUnityPlugin
     {
         public void Awake()
         {
             Logger.LogMessage("Loaded Overhaul");
-            //Acrid Changes
-            Slash.comboFinisherDamageCoefficient = 10;
+            this.crocoBody = Resources.Load<GameObject>("Prefabs/CharacterBodies/CrocoBody");
+            this.CrocoPrimaries();
+            this.CrocoBaseStats();
+        }
 
-            On.EntityStates.Croco.Slash.OnEnter += (orig, self) =>
+        private void AddBodyFlags()
+        {
+            RoR2.CharacterBody component = RoR2.BodyCatalog.FindBodyPrefab("HuntressBody").GetComponent<RoR2.CharacterBody>();
+            component.bodyFlags |= RoR2.CharacterBody.BodyFlags.SprintAnyDirection;
+        }
+
+        private void CrocoPrimaries()
+        {
+            On.EntityStates.Croco.Slash.OnEnter += delegate(On.EntityStates.Croco.Slash.orig_OnEnter orig, EntityStates.Croco.Slash self)
             {
-                Chat.AddMessage("It's ALIVE!");
-                orig(self);
+                self.damageCoefficient = 2.0f;
+                RoR2.Chat.AddMessage("test");
+                self.co
+                orig.Invoke(self);
             };
         }
+
+        private void CrocoBaseStats()
+        {
+            this.crocoBody.GetComponent<RoR2.CharacterBody>().baseMoveSpeed = 10f;
+        }
+
+        public GameObject crocoBody;
     }
 }
